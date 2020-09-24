6e7ee6a02ea677ce757258b7bc3df78220898d34
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-09-24 14:15:16 -0700

    added dropdown hamburger nav w animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { HamburgerCollapseReverse } from 'react-animated-burgers'
 
@@ -7,13 +7,36 @@ import useToggle from '../hooks/useToggle';
 
 const Nav = () => {
     const [ isActive, toggleButton ] = useToggle();
+    const dropDown = useRef();
+    const dropDownBox = useRef();
+
+    useEffect(()=> {
+        const toggleSideMenu = () => {
+            (dropDown.current.className === 'menu active') ? (dropDown.current.className = 'menu hidden') : (dropDown.current.className = 'menu active');
+            console.log(dropDown.current.className);
+        }
+        toggleSideMenu();
+    }, [isActive]);
+
     return (
         <div className="Nav">
             <Link id="home-icon" className="Nav-link" to="/">Shortgrass</Link>
-            <HamburgerCollapseReverse className="Nav-link" buttonWidth={30} isActive={isActive} toggleButton={toggleButton}/>
-            {/* <Link className="Nav-link" to="/about">about</Link>
-            <Link className="Nav-link" to="/digest">digest</Link>
-            <Link className="Nav-link" to="/technology">technology</Link> */}
+            <div className="Nav-drop-down" ref={dropDownBox}>
+                <div>
+                    <HamburgerCollapseReverse className="Nav-link" buttonWidth={30} isActive={isActive} toggleButton={toggleButton}/>
+                </div>
+                <div className="menu active" ref={dropDown}>
+                    <div>
+                        <Link className="Nav-link" to="/technology">Technology</Link>                    
+                    </div>
+                    <div>
+                        <Link className="Nav-link" to="/digest">Digest</Link>                    
+                    </div>
+                    <div>
+                        <Link className="Nav-link" to="/about">About</Link>                    
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
