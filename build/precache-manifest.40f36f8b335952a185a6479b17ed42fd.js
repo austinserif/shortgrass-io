f63d56fa6dd9a5f1634af7c33537dfc2efe212df
Austin Serif <austin@Austins-MacBook-Pro.local>
2020-10-21 15:55:32 -0700

    pushing production

---
+++ build/precache-manifest.40f36f8b335952a185a6479b17ed42fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118b9a378dba1e5b0eaec1518da6fefd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5018193567061d0ea3b8",
+    "url": "/static/css/main.f1141445.chunk.css"
+  },
+  {
+    "revision": "c265e8b9da2d6cacfcda",
+    "url": "/static/js/2.e4b926a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e4b926a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5018193567061d0ea3b8",
+    "url": "/static/js/main.e9117267.chunk.js"
+  },
+  {
+    "revision": "d644692841c679b8c455",
+    "url": "/static/js/runtime-main.85e8292c.js"
+  },
+  {
+    "revision": "2adf41923b6afdf5bdb1f1402f2d26cd",
+    "url": "/static/media/font.2adf4192.woff"
+  },
+  {
+    "revision": "f87f92be15db273dda9d35c2b2b729ab",
+    "url": "/static/media/font.f87f92be.woff2"
+  }
+]);
